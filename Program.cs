b566c00e6da59ce15b991863de13acde2fd7c8f8
Geoff Stockham <geoff@stormid.com>
2016-08-21 18:17:57 +0100

    Add benchmarking

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Diagnostics;
 
 namespace Raytracer
 {
@@ -7,54 +8,83 @@ namespace Raytracer
     {
         static Random random = new Random(DateTime.Now.Millisecond);
 
+        struct RGB 
+        {   
+            public RGB(int r, int g, int b)
+            {
+                R = r;
+                G = g;
+                B = b;
+            }
+
+            public int R {get; set;}
+            public int G {get; set;}
+            public int B {get; set;}
+        }
+
         public static void Main(string[] args)
         {
             int nx = 200;
             int ny = 100;
             int ns = 100;
 
+            var pixels = new RGB[nx,ny];
+
+            var world = new HitableList();
+            world.Add(new Sphere(new Vec3(0, 0, -1), 0.5, new Lambertian(new Vec3(0.1, 0.2, 0.5))));
+            world.Add(new Sphere(new Vec3(0, -100.5, -1), 100, new Lambertian(new Vec3(0.8, 0.8, 0))));
+            world.Add(new Sphere(new Vec3(1, 0, -1), 0.5, new Metal(new Vec3(0.8, 0.6, 0.2), 0.3)));
+            world.Add(new Sphere(new Vec3(-1, 0, -1), 0.5, new Dielectric(1.5)));
+            var camera = new Camera(new Vec3(-2, 2, 1), new Vec3(0, 0, -1), new Vec3(0, 1, 0), 20, (double)nx/(double)ny);
+            Console.WriteLine($"Camera.Origin: {camera.Origin.X} {camera.Origin.Y} {camera.Origin.Z}");
+            Console.WriteLine($"Camera.Horizontal: {camera.Horizontal.X} {camera.Horizontal.Y} {camera.Horizontal.Z}");
+            Console.WriteLine($"Camera.Vertical: {camera.Vertical.X} {camera.Vertical.Y} {camera.Vertical.Z}");
+            Console.WriteLine($"Camera.LowerLeftCorner: {camera.LowerLeftCorner.X} {camera.LowerLeftCorner.Y} {camera.LowerLeftCorner.Z}");
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int j = ny-1; j >= 0; j--)
+            {
+                for (int i = 0; i < nx; i++)
+                {
+
+                    var colour = new Vec3(0, 0, 0);
+
+                    for (int s = 0; s < ns; s++)
+                    {
+                        double u = (i + random.NextDouble()) / nx;
+                        double v = (j + random.NextDouble()) / ny;
+                        var r = camera.GetRay(u, v);
+                        Vec3 p = r.PointAt(2.0);
+                        colour = colour + Colour(r, world, 0);
+                    }
+                    colour = colour / (double)ns;
+                    colour = new Vec3(Math.Sqrt(colour.R), Math.Sqrt(colour.G), Math.Sqrt(colour.B));
+                    int ir = (int)(255.99 * colour.R);
+                    int ig = (int)(255.99 * colour.G);
+                    int ib = (int)(255.99 * colour.B);
+                    pixels[i,j] = new RGB(ir, ig, ib);
+                }
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Rendered in {stopwatch.ElapsedMilliseconds} milliseconds");
             using(var file = File.CreateText("out.ppm"))
             {
                 file.WriteLine($"P3\n{nx} {ny}\n255");
-
-                var world = new HitableList();
-                world.Add(new Sphere(new Vec3(0, 0, -1), 0.5, new Lambertian(new Vec3(0.1, 0.2, 0.5))));
-                world.Add(new Sphere(new Vec3(0, -100.5, -1), 100, new Lambertian(new Vec3(0.8, 0.8, 0))));
-                world.Add(new Sphere(new Vec3(1, 0, -1), 0.5, new Metal(new Vec3(0.8, 0.6, 0.2), 0.3)));
-                world.Add(new Sphere(new Vec3(-1, 0, -1), 0.5, new Dielectric(1.5)));
-                var camera = new Camera(new Vec3(-2, 2, 1), new Vec3(0, 0, -1), new Vec3(0, 1, 0), 20, (double)nx/(double)ny);
-                Console.WriteLine($"Camera.Origin: {camera.Origin.X} {camera.Origin.Y} {camera.Origin.Z}");
-                Console.WriteLine($"Camera.Horizontal: {camera.Horizontal.X} {camera.Horizontal.Y} {camera.Horizontal.Z}");
-                Console.WriteLine($"Camera.Vertical: {camera.Vertical.X} {camera.Vertical.Y} {camera.Vertical.Z}");
-                Console.WriteLine($"Camera.LowerLeftCorner: {camera.LowerLeftCorner.X} {camera.LowerLeftCorner.Y} {camera.LowerLeftCorner.Z}");
                 for (int j = ny-1; j >= 0; j--)
                 {
                     for (int i = 0; i < nx; i++)
                     {
-
-                        var colour = new Vec3(0, 0, 0);
-
-                        for (int s = 0; s < ns; s++)
-                        {
-                            double u = (i + random.NextDouble()) / nx;
-                            double v = (j + random.NextDouble()) / ny;
-                            var r = camera.GetRay(u, v);
-                            Vec3 p = r.PointAt(2.0);
-                            colour = colour + Colour(r, world, 0);
-                        }
-                        colour = colour / (double)ns;
-                        colour = new Vec3(Math.Sqrt(colour.R), Math.Sqrt(colour.G), Math.Sqrt(colour.B));
-                        int ir = (int)(255.99 * colour.R);
-                        int ig = (int)(255.99 * colour.G);
-                        int ib = (int)(255.99 * colour.B);
-                        file.WriteLine($"{ir} {ig} {ib}");
+                        var pixel = pixels[i,j];
+                        file.WriteLine($"{pixel.R} {pixel.G} {pixel.B}");
                     }
-                    //Console.WriteLine($"Row: {j}");
-               }
+                }
             }
+
+            
         }
 
-         private static Vec3 Colour(Ray r, Hitable world, int depth)
+        private static Vec3 Colour(Ray r, Hitable world, int depth)
         {
             HitRecord rec;
             if(world.Hit(r, 0.001, Double.MaxValue, out rec))
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # raytracer-cs-core
 Raytracing in One Weekend - .Net Core C# version
+
+This implements as far as the moveable camera, but not depth of field.
+
+For benchmarking against my C++ version (https://github.com/gjstockham/raytracer-cpp) this version currently runs in 13500 ms (debug) and 3255 ms (release)
